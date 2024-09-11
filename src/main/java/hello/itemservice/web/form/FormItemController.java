71442a5be1cda2b53b7ca6773482d5f8b3cdc422
seonhoshin <sunlike0508@gmail.com>
2024-09-11 20:55:18 +0900

    선택변수식

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,14 +1,17 @@
 package hello.itemservice.web.form;
 
+import java.util.List;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/form/items")
@@ -17,12 +20,14 @@ public class FormItemController {
 
     private final ItemRepository itemRepository;
 
+
     @GetMapping
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "form/items";
     }
+
 
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
@@ -31,10 +36,13 @@ public class FormItemController {
         return "form/item";
     }
 
+
     @GetMapping("/add")
-    public String addForm() {
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
         return "form/addForm";
     }
+
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
@@ -44,12 +52,14 @@ public class FormItemController {
         return "redirect:/form/items/{itemId}";
     }
 
+
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "form/editForm";
     }
+
 
     @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
